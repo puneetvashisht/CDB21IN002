5c094a5d11e8906b23f431bc5db7384d5b983e8e
Puneet <puneetvashisht@gmail.com>
2021-04-27 16:45:49 +0530

    server side communication

--- react/my-second-app/src/App.js
+++ react/my-second-app/src/App.js
@@ -9,19 +9,29 @@ export default class App extends Component {
   constructor(){
     super();
     this.state = {courses: [
-      {title: "Angular", summmary: 'framework from google'},
-      {title: "React", summmary: 'library from facebook'},
-      {title: "Ember", summmary: 'framework oper-source'},
-      {title: "Angular", summmary: 'framework from google'},
-      {title: "React", summmary: 'library from facebook'}
+      // {title: "Angular", summmary: 'framework from google'},
+      // {title: "React", summmary: 'library from facebook'},
+      // {title: "Ember", summmary: 'framework oper-source'},
+      // {title: "Angular", summmary: 'framework from google'},
+      // {title: "React", summmary: 'library from facebook'}
     ]}
   }
+
+  componentDidMount() {
+    fetch('http://localhost:3004/courses')
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+      this.setState({courses: data})
+    })
+  }
+  
 
   render() {
 
     let courseList = this.state.courses.map((course)=>{
       return (
-        <Card caption= {course.title} desc={course.summmary}></Card>
+        <Card caption= {course.title} desc={course.summmary} id={course.id}></Card>
       )
     })
 
